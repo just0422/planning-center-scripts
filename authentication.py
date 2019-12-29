c482eb91232859ec6a1c436ef2ec733746afe992
Justin Maldonado <justin.maldonado@stonybrook.edu>
2019-12-29 12:07:52 -0500

    Auth file formatting

--- authentication.py
+++ authentication.py
@@ -2,15 +2,16 @@ import urllib
 import json
 import requests
 
-from rauth import *
+from rauth import OAuth1Session, OAuth1Service
 from base64 import b64encode
+
 
 class F1API(OAuth1Session):
     """A class for initializaint the F1 api session"""
-    
+
     def __init__(self, clientKey, clientSecret, username, password):
         """Class Constructor assigns class variables and attempts authentication
-        
+
         :param clientKey: Oauth1 client key
         :type clientKey: string
         :param clientSecret: Oauth1 client secret
@@ -26,31 +27,36 @@ class F1API(OAuth1Session):
         # Hash credentials
         credential_string = "{} {}".format(username, password)
         credentials = b64encode(bytes(credential_string, "utf-8"))
-        credentials = urllib.parse.quote_plus( credentials )
+        credentials = urllib.parse.quote_plus(credentials)
 
         # Setup OAuth1 Service to retrive Request Tokens
-        service = OAuth1Service (
-           consumer_key = client_key,
-           consumer_secret = client_secret,
-           request_token_url = "%s/v1/PortalUser/AccessToken" % self.baseUrl
+        service = OAuth1Service(
+           consumer_key=clientKey,
+           consumer_secret=clientSecret,
+           request_token_url="%s/v1/PortalUser/AccessToken" % self.baseUrl
         )
-        
+
         # Fetch tokens from fellowship one
-        tokens = service.get_raw_request_token(data = credentials)
+        tokens = service.get_raw_request_token(data=credentials)
         tokens_content = urllib.parse.parse_qs(tokens.content)
 
         # Parse Ouath Request token and Secret
         oauth_token = tokens_content[b'oauth_token'][0].decode()
         oauth_tokensecret = tokens_content[b'oauth_token_secret'][0].decode()
-        
+
         # Create a session that will be used for every following request
-        session = OAuth1Session (clientKey, clientSecret, oauth_token, oauth_tokensecret)
+        session = OAuth1Session(
+                clientKey,
+                clientSecret,
+                oauth_token,
+                oauth_tokensecret
+        )
         self.session = session
 
     def get(self, endpoint, urlParams, **kwargs):
         for key in urlParams:
-            path = path.replace('{{{}}}'.format(key), str(urlParams[key]))
-	request_url = "%s%s" % (self.baseUrl, self.clean_endpoint(endpoint))
+            endpoint = endpoint.replace('{{{}}}'.format(key), str(urlParams[key]))
+        request_url = "%s%s" % (self.baseUrl, self.clean_endpoint(endpoint))
 
-	return self.session.get(request_url, header_auth=True, headers={"Accept": "application/json"}, **kwargs)
+        return self.session.get(request_url, header_auth=True, headers={"Accept": "application/json"}, **kwargs)
 
