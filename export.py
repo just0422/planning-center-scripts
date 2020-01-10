bf0fcc00d38f5fa51f05720971663110fe9db0d8
Justin Maldonado <justin.maldonado@stonybrook.edu>
2020-01-10 18:49:47 -0500

    Created a person object

--- export.py
+++ export.py
@@ -1,12 +1,16 @@
 #!/usr/local/bin/python3
+import pypco
 import sqlite3
-import string
 import sys
 
 from sqlite3 import Error
+from utils.person import Person
 
-from dateutil.relativedelta import relativedelta
-import datetime
+
+pco = pypco.PCO(
+        "",
+        ""
+    )
 
 
 def create_connection(db_file):
@@ -29,65 +33,30 @@ def compare(first, second):
     )
 
 
-def is_a_duplicate(row, rows, index):
-    # row without UIDs
-    first_name = row[4]
-    last_name = row[3]
-    dob = row[8]
-    address1 = row[10]
-    address2 = row[11]
-    pref_phone = row[12]
-    mobile_phone = row[13]
-    pref_email = row[14]
-    email = row[15]
-
+def is_a_duplicate(person, rows, index):
     for check_row in rows[index - 20: index + 20]:
         if row == check_row:
             return False
 
-        cr_first_name = check_row[4]
-        cr_last_name = check_row[3]
-        cr_dob = check_row[8]
-        cr_address1 = check_row[10]
-        cr_address2 = check_row[11]
-        cr_pref_phone = check_row[12]
-        cr_mobile_phone = check_row[13]
-        cr_pref_email = check_row[14]
-        cr_email = check_row[15]
+        check_row_person = Person(check_row)
 
         # Skip if the name is different
-        if first_name != cr_first_name:
+        if person.first_name != check_row_person.first_name:
             continue
-        if last_name != cr_last_name:
+        if person.last_name != check_row_person.last_name:
             continue
 
         # Check DOB, address, email, or mobile_phone
-        if (compare(dob, cr_dob) or
-                compare(address1, cr_address1) or
-                compare(address2, cr_address2) or
-                compare(mobile_phone, cr_mobile_phone) or
-                compare(pref_phone, cr_pref_phone) or
-                compare(email, cr_email) or
-                compare(pref_email, cr_pref_email)):
+        if (compare(person.dob, check_row_person.dob) or
+                compare(person.address1, check_row_person.address1) or
+                compare(person.address2, check_row_person.address2) or
+                compare(person.mobile_phone, check_row_person.mobile_phone) or
+                compare(person.pref_phone, check_row_person.pref_phone) or
+                compare(person.email, check_row_person.email) or
+                compare(person.pref_email, check_row_person.pref_email)):
             return True
 
     return False
-
-
-def is_a_bad_word(word):
-    invalid_chars = string.punctuation
-    invalid_chars = invalid_chars.replace(".", "")
-    invalid_chars = invalid_chars.replace("(", "")
-    invalid_chars = invalid_chars.replace(")", "")
-    return (
-        len(word.replace(".", "")) <= 1 or
-        word.count('.') >= 2 or
-        word[0] == '(' or
-        word[len(word) - 1] == ')' or
-        any(char.isdigit() for char in word) or
-        any(char in set(invalid_chars) for char in word) or
-        len(word.split(' ')) > 3
-    )
 
 
 if __name__ == '__main__':
@@ -99,36 +68,23 @@ if __name__ == '__main__':
 
         rows = cursor.fetchall()
 
-        print(len(rows))
-
         valid = 0
         lap = 0
         dups = 0
-        lasts = 0
-        firsts = 0
-
-        kids = 0
+        names = 0
 
         for row in rows[1:]:
+            person = Person(row)
+
             lap += 1
             sys.stdout.write('\rProfile Count: {0}'.format(lap))
             sys.stdout.flush()
 
-            first_name = row[4].strip()
-            last_name = row[3].strip()
-
-            if is_a_bad_word(first_name):
-                # print(row)
-                firsts += 1
+            if person.has_a_bad_name():
+                names += 1
                 continue
 
-            if is_a_bad_word(last_name):
-                # print(row)
-                lasts += 1
-                continue
-
-            if is_a_duplicate(row, rows, lap):
-                # print(row)
+            if is_a_duplicate(person, rows, lap):
                 dups += 1
                 continue
 
@@ -137,8 +93,8 @@ if __name__ == '__main__':
         print('\n\n')
         print("Valid profiles: " + str(valid))
         print("Duplicates: " + str(dups))
-        print("Bad First names: " + str(firsts))
-        print("Bad Last Names: " + str(lasts))
+        print("Bad Names: " + str(names))
+        # print("Bad Last Names: " + str(lasts))
     finally:
         if conn:
             conn.close()
---
+++ utils/person.py
@@ -0,0 +1,37 @@
+import string
+
+
+class Person:
+    """Representation of a person from F1 using the SQLite representation"""
+    def __init__(self, obj):
+        self.id = obj[0]
+        self.household_id = obj[1]
+
+        self.first_name = (obj[4] or '').strip()
+        self.last_name = (obj[3] or '').strip()
+        self.dob = (obj[8] or '').strip()
+        self.address1 = (obj[10] or '').strip()
+        self.address2 = (obj[11] or '').strip()
+        self.pref_phone = (obj[12] or '').strip()
+        self.mobile_phone = (obj[13] or '').strip()
+        self.pref_email = (obj[14] or '').strip()
+        self.email = (obj[15] or '').strip()
+
+    def has_a_bad_name(self):
+        return (self.is_a_bad_word(self.first_name) or
+                self.is_a_bad_word(self.last_name))
+
+    def is_a_bad_word(self, word):
+        invalid_chars = string.punctuation
+        invalid_chars = invalid_chars.replace(".", "")
+        invalid_chars = invalid_chars.replace("(", "")
+        invalid_chars = invalid_chars.replace(")", "")
+        return (
+            len(word.replace(".", "")) <= 1 or
+            word.count('.') >= 2 or
+            word[0] == '(' or
+            word[len(word) - 1] == ')' or
+            any(char.isdigit() for char in word) or
+            any(char in set(invalid_chars) for char in word) or
+            len(word.split(' ')) > 3
+        )
